ed1d1e3e4c3c5952ff25e39609101a2c377238ab
Petr <pmarfutenko3010@gmail.com>
2022-07-30 13:30:27 +0500

    Commit for semen

--- src/pages/Search/SearchPage.jsx
+++ src/pages/Search/SearchPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import MainSearch from "../../components/MainSearch/MainSearch";
+import TopCities from "../../components/TopCities/TopCities";
 
 const SearchPage = () => {
     return (
         <div>
             <MainSearch/>
+            <TopCities/>
         </div>
     );
 };
